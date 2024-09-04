4fa95f8dee0d66969cce21dfc11a53ab5eed865f
yeon-dong <jae073@naver.com>
2024-09-04 11:29:49 +0900

    vue search 개발

--- FE_Study/04_VUE/vuetest2/src/App.vue
+++ FE_Study/04_VUE/vuetest2/src/App.vue
@@ -1,6 +1,8 @@
 <template>
    <NavbarView :activeIndex="activeIndex" @changeActive="setActiveIndex" />
-   <CardView v-bind:data="data" :isModal="isModal" @modalOpen="modalOpen" @increaseLike="increaseLike"></CardView>
+   <SearchBarView :data="data_temp" @searchMovie="searchMovie" />
+   <button @click="viewAllMovie">전체보기</button>
+   <CardView v-bind:data="data_temp" :isModal="isModal" @modalOpen="modalOpen" @increaseLike="increaseLike"></CardView>
    <ModalView
       v-bind:mydata="data"
       :isModal="isModal"
@@ -14,6 +16,7 @@ import mdata from '@/assets/mdata';
 import NavbarView from '@/components/NavbarView.vue';
 import ModalView from '@/components/ModalView.vue';
 import CardView from '@/components/CardView.vue';
+import SearchBarView from './components/SearchBarView.vue';
 
 export default {
    name: 'appView',
@@ -22,26 +25,37 @@ export default {
          data: mdata,
          isModal: false,
          selectedNum: 0,
-         activeIndex: 0, // 추가: 현재 활성화된 메뉴 인덱스
+         data_temp: [...mdata],
       };
    },
    methods: {
-      increaseLike(i) {
-         this.data[i].like = this.data[i].like + 1;
+      increseLike(i) {
+         console.log(i);
+         this.data[i].like += 1;
       },
       modalOpen(num) {
          this.isModal = true;
          this.selectedNum = num;
+         console.log('view', num);
       },
-      setActiveIndex(index) {
-         // 추가: 활성화된 인덱스 설정 메서드
-         this.activeIndex = index;
+      closeM() {
+         this.isModal = false;
+      },
+      searchMovie(title) {
+         console.log('영화이름은 ' + title);
+         this.data_temp = this.data.filter(movie => {
+            return movie.title.includes(title);
+         });
+      },
+      viewAllMovie() {
+         this.data_temp = [...this.data];
       },
    },
    components: {
-      NavbarView,
-      ModalView,
-      CardView,
+      NavbarView: NavbarView,
+      ModalView: ModalView,
+      CardView: CardView,
+      SearchBarView: SearchBarView,
    },
 };
 </script>
---
+++ FE_Study/04_VUE/vuetest2/src/components/SearchBarView.vue
@@ -0,0 +1,51 @@
+<template>
+   <div>
+      <!-- <input type="search" @input="inputText = $event.target.value" placeholder="검색어를 입력하세요" /> -->
+      <!-- <input type="search" v-model="inputText" placeholder="검색어를 입력하세요" /> -->
+      <!-- <input
+         type="search"
+         @change="
+            inputText = $event.target.value;
+            $event.target.value = '';
+         "
+      /> -->
+      <input type="search" @change="handleSearch" placeholder="검색어를 입력하세요" />
+      <button>검색</button>
+      <!-- <p>{{ inputText }}</p> -->
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'SearchBar',
+   props: {
+      data: Object,
+   },
+   data() {
+      return {
+         inputText: 'test',
+      };
+   },
+   methods: {
+      handleSearch(event) {
+         this.$emit('searchMovie', event.target.value);
+         this.inputText = event.target.value;
+         event.target.value = '';
+      },
+   },
+   watch: {
+      inputText(name) {
+         console.log('watch' + name);
+         const findName = this.data.filter(movie => {
+            return movie.title.includes(name);
+         });
+         console.log(findName.length);
+         if (findName.length == 0) {
+            alert('영화자료가 없습니다.!!!');
+         }
+      },
+   },
+};
+</script>
+
+<style lang="scss" scoped></style>
