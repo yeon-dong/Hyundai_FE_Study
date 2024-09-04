97f07bdb6a1ba325df1bc9eedabfc43b0a65e888
yeon-dong <jae073@naver.com>
2024-09-04 17:14:20 +0900

    vue api공부중

--- FE_Study/04_VUE/vuetest3/src/App.vue
+++ FE_Study/04_VUE/vuetest3/src/App.vue
@@ -1,41 +1,49 @@
 <template>
-  <div>{{ count }} {{ text }}</div>
-  <button @click="addNum">클릭</button>
-  <ViewComp :text="text" @update="textChange"></ViewComp>
+  <div>
+    <NavbarView />
+    <SearchbarView @searchCity="searchCity" />
+    <WeatherView :data="weatherData" />
+
+    <!-- {{ weatherData }} -->
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
-import ViewComp from "./components/ViewComp.vue";
+import SearchbarView from "./components/SearchbarView.vue";
+import WeatherView from "./components/WeatherView.vue";
 import axios from "axios";
 
-const count = ref(0);
-const text = ref("title");
 const weatherData = ref({
   icon: "icon",
   temp: 0,
   text: "text",
   location: "location",
-  city: "seoul",
+  city: "jeju",
 });
-const addNum = () => {
-  count.value++;
-};
-
-const textChange = () => {
-  text.value = "vueComp";
-};
 
 const getWeather = async () => {
   const res = await axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=${weatherData.value.city}&appid=4eedfeb184dc7cb08af6c0bd529c48b9`
   );
   console.log(res.data);
+
+  weatherData.value.icon = res.data.weather[0].icon;
+  weatherData.value.temp = res.data.main.temp;
+  weatherData.value.text = res.data.weather[0].description;
+  weatherData.value.location = res.data.sys.country;
+  weatherData.value.city = res.data.name;
 };
 
 onMounted(() => {
   getWeather();
 });
+
+const searchCity = (city) => {
+  console.log(city);
+  weatherData.value.city = city;
+  getWeather();
+};
 </script>
 
 <style lang="scss" scoped></style>
