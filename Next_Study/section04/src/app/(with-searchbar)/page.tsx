8377e0fc7e1e080c2b681cb2f1dac6fde410e283
yeon-dong <jae073@naver.com>
2025-01-03 11:50:45 +0900

    데이터 페칭 공부 중

--- Next_Study/section04/src/app/(with-searchbar)/page.tsx
+++ Next_Study/section04/src/app/(with-searchbar)/page.tsx
@@ -5,6 +5,10 @@ import { BookData } from "@/types";
 
 async function AllBooks() {
   const response = await fetch(`http://localhost:12345/book`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+
   const allBooks: BookData[] = await response.json();
 
   return (
@@ -16,7 +20,22 @@ async function AllBooks() {
   );
 }
 
-async function RecoBooks() {}
+async function RecoBooks() {
+  const response = await fetch(`http://localhost:12345/book/random`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+
+  const recoBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
 
 export default async function Home() {
   const response = await fetch(`http://localhost:12345/book`);
@@ -26,9 +45,7 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
