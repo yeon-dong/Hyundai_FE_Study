2e923c7cedb4430e16888e41591cbd21b9dcfa98
yeon-dong <jae073@naver.com>
2025-01-05 16:30:49 +0900

    데이터 페칭 공부 중

--- Next_Study/section04/src/app/(with-searchbar)/page.tsx
+++ Next_Study/section04/src/app/(with-searchbar)/page.tsx
@@ -4,7 +4,9 @@ import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
 async function AllBooks() {
-  const response = await fetch(`http://localhost:12345/book`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
   }
@@ -21,7 +23,9 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
-  const response = await fetch(`http://localhost:12345/book/random`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
   }
