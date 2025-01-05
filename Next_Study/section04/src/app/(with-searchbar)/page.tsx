6e5e35c49be8e53d54b9ddb3edffe101c40ea119
yeon-dong <jae073@naver.com>
2025-01-05 20:05:05 +0900

    라우트 세그먼트 옵션 공부

--- Next_Study/section04/src/app/(with-searchbar)/page.tsx
+++ Next_Study/section04/src/app/(with-searchbar)/page.tsx
@@ -3,6 +3,13 @@ import style from "./page.module.css";
 import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
+export const dynamic = "auto";
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본 값, 아무 것도 강제하지 않음
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static : 페이지를 강제로 Static 페이지로 설정 -> 쿼리스트링이 있는 경우 검색 기능이 제대로 동작 안 할 수 있음.
+// 4. error : 페이지를 강제로 Static 페이지로 설정하지만 설정 하면 안되는 이유가 있으면 -> 빌드오류
+
 async function AllBooks() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
