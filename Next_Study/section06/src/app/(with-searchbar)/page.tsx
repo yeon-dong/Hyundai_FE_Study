f5c1ec43bda8f495366b974b54783bc28d6985fc
yeon-dong <jae073@naver.com>
2025-01-06 12:02:13 +0900

    스켈레톤 UI 적용

--- Next_Study/section06/src/app/(with-searchbar)/page.tsx
+++ Next_Study/section06/src/app/(with-searchbar)/page.tsx
@@ -2,8 +2,12 @@ import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import books from "@/mock/books.json";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
+import BookItemSkeleton from "@/components/skeleton/book-item-skeleton";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 
-export const dynamic = "auto";
+export const dynamic = "force-dynamic"; //잠깐 suspense를 보려고 설정해둠둠
 // 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
 // 1. auto : 기본 값, 아무 것도 강제하지 않음
 // 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
@@ -11,6 +15,7 @@ export const dynamic = "auto";
 // 4. error : 페이지를 강제로 Static 페이지로 설정하지만 설정 하면 안되는 이유가 있으면 -> 빌드오류
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
@@ -31,6 +36,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -58,11 +64,27 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense
+          fallback={
+            <>
+              <BookListSkeleton count={3} />
+            </>
+          }
+        >
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense
+          fallback={
+            <>
+              <BookListSkeleton count={3} />
+            </>
+          }
+        >
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- Next_Study/section06/src/app/(with-searchbar)/search/page.tsx
+++ Next_Study/section06/src/app/(with-searchbar)/search/page.tsx
@@ -2,16 +2,11 @@ import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{
-    q?: string;
-  }>;
-}) {
+async function SearchResult({ q }: { q: string }) {
   await delay(1500); // 일부러 지연 시키려고 만든 비동기 함수
-  const { q } = await searchParams;
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" }
@@ -28,5 +23,20 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    q?: string;
+  }>;
+}) {
+  const { q } = await searchParams;
+  return (
+    <Suspense key={q || ""} fallback={<BookListSkeleton count={3} />}>
+      <SearchResult q={q || ""} />
+    </Suspense>
   );
 }
